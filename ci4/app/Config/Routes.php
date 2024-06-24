2bf81bb120b7c2f517fc3d3827b1d8930229762b
Elijah Bravo <elbravo@student.apc.edu.ph>
2024-06-25 03:46:20 +0800

    Rest API Part 1

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -4,8 +4,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
- */
-$routes->get('/', 'Home::index');
+
+
 
 use App\Controllers\Articles; // Add this line
 use App\Controllers\News; 
